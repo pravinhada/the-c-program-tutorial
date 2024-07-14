348adfd279b83e364c0834e97d529f3750b3b0fa
Prabin Hada <phada@Prabins-MacBook-Pro.local>
2024-07-14 12:20:46 -0400

    refactor

--- pointers_and_arrays/turing_maching.c
+++ pointers_and_arrays/turing_maching.c
@@ -1,13 +1,27 @@
 #include <stdio.h>
+#include <string.h>
+
+/*
+42 > 114 > 105 > 97 >
+110 < < < < 66
+
+ will be output as:
+ Memory:
+ Brian
+ */
 
 int main() {
     char memory[256], token[256];
     int position = 0, value;
-
     while (scanf("%s", token) == 1) {
-        printf("%s\n", token);
-
-        /* Replace this with your code */
+        if (strcmp(token, "<") == 0)
+            position--;
+        else if (strcmp(token, ">") == 0)
+            position++;
+        else {
+            sscanf(token, "%d", &value);
+            memory[position] = value;
+        }
     }
     printf("Memory:\n%s\n", memory);
 }
