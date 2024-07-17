99600af330baa492a0bf7dfb384849d0c3cf6d03
Prabin Hada <phada@Prabins-MacBook-Pro.local>
2024-07-17 19:11:32 -0400

    adding search word

---
+++ structure/search_word.c
@@ -0,0 +1,87 @@
+#include <stdio.h>
+#include <ctype.h>
+#include <string.h>
+#include "../functions_programs/calculator/getch.c"
+
+#define MAXWORD 100
+#define NKEYS 13
+
+struct key {
+    char *word;
+    int count;
+} keytab[] = {
+        "auto", 0,
+        "break", 0,
+        "case", 0,
+        "char", 0,
+        "const", 0,
+        "continue", 0,
+        "default", 0,
+        "unsigned", 0,
+        "void", 0,
+        "volatile", 0,
+        "while", 0,
+        "else", 0,
+        "enum", 0};
+
+int getword(char *, int);
+
+int binsearch(char *, struct key *, int);
+
+int main() {
+    int n;
+    char word[MAXWORD];
+
+    while (getword(word, MAXWORD) != EOF)
+        if (isalpha(word[0]))
+            if ((n = binsearch(word, keytab, NKEYS)))
+                keytab[n].count++;
+
+    for (n = 0; n < NKEYS; n++)
+        if (keytab[n].count > 0)
+            printf("%4d %s\n", keytab[n].count, keytab[n].word);
+
+    return 0;
+}
+
+int getword(char *word, int n) {
+    int c;
+    char *w = word;
+
+    while (isspace(c = getch()));
+
+    if (c != EOF)
+        *w++ = c;
+    if (!isalpha(c)) {
+        *w = '\0';
+        return c;
+    }
+
+    for (; --n > 0; w++) {
+        if (!isalpha(*w = getch())) {
+            ungetch(*w);
+            break;
+        }
+    }
+
+    *w = '\0';
+    return word[0];
+}
+
+int binsearch(char *word, struct key tab[], int n) {
+    int cond;
+    int low, high, mid;
+    low = 0;
+    high = n - 1;
+
+    while (low <= high) {
+        mid = (low + high) / 2;
+        if ((cond = strcmp(word, tab[mid].word)) < 0)
+            high = mid - 1;
+        else if (cond > 0)
+            low = mid + 1;
+        else
+            return mid;
+    }
+    return -1;
+}
