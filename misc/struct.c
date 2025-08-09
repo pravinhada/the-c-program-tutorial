24f86bd9eeaa9ecc2f6a218b21ebe6e40f73acc2
phada <phada@globallink.com>
2025-08-09 18:41:03 -0400

    use typedef

--- misc/struct.c
+++ misc/struct.c
@@ -1,24 +1,24 @@
 #include <stdio.h>
 
-struct Point {
+typedef struct {
     float x;    // 4 bytes
     float y;    // 4 bytes
-};
+} Point;
 
-struct Point p1, p2;
+Point p1, p2;
 
-void printPoint(const struct Point p) {
+void printPoint(Point p) {
     printf("x = %f, y = %f\n", p.x, p.y);
 }
 
 // pass by value
-void changePoint(struct Point p) {
+void changePoint(Point p) {
     p.x = 15;
     p.y = 25;
 }
 
 // pass by reference
-void changePointAgain(struct Point *p) {
+void changePointAgain(Point *p) {
     p->x = 30;
     p->y = 40;
 }
@@ -34,5 +34,5 @@ int main() {
     printPoint(p1);
 
     printf("Size of Point is %lu bytes\n", sizeof(p1));
-    printf("Size of Point struct is %lu bytes\n", sizeof(struct Point));
+    printf("Size of Point struct is %lu bytes\n", sizeof(Point));
 }
